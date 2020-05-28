94ea23eb699b81039d7465212b96eb718bbcd12b
shamarni mcfarlane <Smcfarlane926@gmail.com>
2020-05-28 15:50:50 +0100

    update

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ function addToWall(list){
         let newContent = document.createTextNode(item)
         newLi.appendChild(newContent)
         let currentUl = document.getElementById("list")
-        document.body.insertBefore(newLi, currentUl)
     })
+}
+
+function addToList(){
+ let items = document.getElementById("add")[0].value;
+    fetch('https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall', {
+  method: 'POST',
+  body: JSON.stringify({message: value})
+}).then(function (response) {
+  if (response.status == 200) {
+    console.log("Ok!")
+  } else {console.log("Oh no!")}
+})
 }
